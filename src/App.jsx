46abc69b494035dc66a757f8608c37bd7c4b6028
Tauhid hossen <tauhidhossen04@gmail.com>
2024-11-25 23:40:11 +0600

    get data from server

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,44 @@ function App() {
     .then(data => setUsers(data))
   },[])
 
+  
+
+  const handleUser = event => {
+    event.preventDefault();
+    const from = event.target;
+    const name = from.name.value;
+    const email = from.email.value;
+    const user = {name,email}
+    console.log(user);
+    
+    fetch('http://localhost:5000/users',{
+      method:'POST',
+      headers : {
+        'content-type' : 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      const newUsers = [...users,data];
+      setUsers(newUsers);
+      setUsers(newUsers);
+      from.reset();
+      
+    })
+  }
+
   return (
     <>
-      
+     <form onSubmit={handleUser}>
+
+      <input type="text" name="name" id="" />
+      <br />
+      <input type="email" name="email" id="" />
+      <br />
+      <input type="submit" value="Add User" />
+     </form>
       <h1>Users management System : {users.length}</h1>
       <div>
     {users.map(user => <p key={user.id} > {user.id} : {user.name} : {user.email}</p>)}
